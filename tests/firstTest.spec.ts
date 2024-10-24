893e9c732369b973d9d5b29129dbefb2bb7eb768
pabs28 <pablosapitan28@gmail.com>
2024-10-24 13:59:55 +0800

    Add test for reference

--- tests/firstTest.spec.ts
+++ tests/firstTest.spec.ts
@@ -4,12 +4,28 @@ test.beforeEach(async ({page}) => {
     await page.goto('https://console-test.remtrax.com/login');
 })
 
-test('The first test', async ({page}) => {  
-    await page.getByText('Sign In').click();
+test('User facing locator', async ({page}) => {  
+    await page.getByRole('textbox', { name: 'E-mail' }).fill('pablo.sapitan@live.com');
+    await page.getByRole('textbox', { name: 'Password' }).fill('password');
+    await page.getByRole('button', { name: 'Sign in' }).click();
+    await page.waitForTimeout(5000);
 })
 
-test('Navigate to page ', async ({page}) => {  
-    await page.goto('https://console-test.remtrax.com/login');
-    await page.getByText('Sign In').click();
-    
+test('Locating child elements', async ({page}) => {  
+    await page.locator('nb-card nb-radio :text-is("Option 1")').click();
+    await page.locator('nb-card').locator('nb-radio').locator(':text-is("Option 2")').click();
+
+    await page.locator('nb-card').getByRole('button', {name: "Sign in"}).first().click();
+})
+
+test('locating parent elements', async({page}) => {
+    await page.locator('nb-card', {hasText: "Using the Grid"}).getByRole('textbox', {name: 'E-mail'}).click();
+    await page.locator('nb-card', {has: page.locator('#inputEmail')}).getByRole('textbox', {name: 'E-mail'}).click();
+
+    await page.locator('nb-card').filter({hasText: "Basic form"}).getByRole('textbox', {name: 'E-mail'}).click();
+    await page.locator('nb-card').filter({has: page.locator('.status-danger')}).getByRole('textbox', {name: 'E-mail'}).click();
+
+    await page.locator('nb-card').filter({has: page.locator('nb-checkbox')}).filter({hasText: "Sign In"}).getByRole('textbox', {name: "Email"}).click();
+
+    await page.locator(':text-is("Using the Grid")').locator('..').getByRole('textbox', {name: "Email"}).click();
 })
